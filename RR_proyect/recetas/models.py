46377479311e4c82309c1fe5f1630b54b8290ddd
JaimeAbuin51 <avmjaime@gmail.com>
2026-05-26 14:06:00 +0200

    Modelos receta y categoria añadidos

--- RR_proyect/recetas/models.py
+++ RR_proyect/recetas/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Receta(models.Model):
+    titulo = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    pasos = models.TextField()
+    tiempo_preparacion = models.PositiveIntegerField(help_text="Tiempo en minutos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
