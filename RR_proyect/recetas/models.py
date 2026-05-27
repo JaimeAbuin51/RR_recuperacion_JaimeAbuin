432a38101eeef8cf9f9e9b3d49e90aa0df14b208
JaimeAbuin51 <avmjaime@gmail.com>
2026-05-27 12:14:03 +0200

    Ahora las recetas se ordenan por fecha

--- RR_proyect/recetas/models.py
+++ RR_proyect/recetas/models.py
@@ -16,6 +16,8 @@ class Receta(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
+    class Meta:
+    ordering = ['-fecha_creacion']
 
     def __str__(self):
         return self.titulo
